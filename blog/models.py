8cc9b783c3fe881ef1923f7084883edac6d78838
Navid Dezashibi <dezashibi@gmail.com>
2020-08-03 19:55:45 +0430

    completed

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    date = models.DateField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
